7621c6cee2efaed002b55ac7882c46615d55a658
Nico Orlando <nico.orlando@dave.eu>
2025-03-25 11:33:19 +0100

    files: nvim: add neo-tree

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -1,3 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.opt.relativenumber = true
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+}
